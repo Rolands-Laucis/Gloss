1076c58246ca0208b42fa3316de96ef52fd65879
Rolands <rolands.laucis@gmail.com>
2025-07-12 11:51:02 +0300

    build optim

--- b.ps1
+++ b.ps1
@@ -1,8 +1,23 @@
-npm run tauri build
-# only move the files, if the build didnt error
+# Corrected build command for Tauri
+npm run tauri build -- --no-bundle
+
+# Only move the files if the build didn't error
 if ($?) {
+    Write-Output "deleting unneeded build folders..."
+    # Delete specific folders if they exist
+    # $foldersToDelete = @("build", "bundle", "examples", "incremental", "nsis", "wix", "deps")
+    $foldersToDelete = @("bundle", "examples", "incremental", "nsis", "wix", "deps")
+    foreach ($folder in $foldersToDelete) {
+        $path = "src-tauri\target\release\$folder"
+        if (Test-Path $path) {
+            Remove-Item $path -Recurse -Force
+        }
+    }
+
     Write-Output "moving release folder..."
     Copy-Item src-tauri\target\release -Destination F:\Gloss -Recurse -Force
+    
+    if (Test-Path "F:\Gloss\release\build") {
+        Remove-Item "F:\Gloss\release\build" -Recurse -Force
+    }
 }
-
-# Move-Item src-tauri\target\release F:\Muse -Force
